3476b529a309ee7c8ef7e93957552b0698e84b65
Nayonika <nayonika.np@gmail.com>
2026-03-22 20:35:21 +0530

    updated for deployment

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, request, jsonify, render_template
 import pickle
-import numpy as np
 
 app = Flask(__name__)
-CORS(app)
 
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return "Fraud Detection Running 🚀"
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
